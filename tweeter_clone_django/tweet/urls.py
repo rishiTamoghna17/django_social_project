34be7a903e4f4ed60c22a0fdb8c3a14176a14741
rishiTamoghna17 <tamoghna171099@gmail.com>
2024-10-27 21:43:16 +0530

    Initial commit

---
+++ tweeter_clone_django/tweet/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+urlpatterns = [
+    path('', views.tweet_list, name='tweet_list'),
+    path('create/', views.create_tweet, name='create_tweet'),
+    path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+    # user functionality
+    path('register/', views.register, name='register'),
+
+    
+]
